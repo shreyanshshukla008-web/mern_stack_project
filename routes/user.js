cbdb49744182ab4d4f80ae0e52ee71d3e615dd0e
Shreyansh Shukla <230330341+shreyanshshukla008-web@users.noreply.github.com>
2026-08-14 22:36:23 +0530

    routes for signup and login

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,10 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require('passport');
 const { saveRedirectUrl } = require('../middleware.js');
 const userController = require("../controllers/users.js");
-
 router
 .route("/signup")
 .get(userController.renderSignupForm)
 .post(wrapAsync(userController.signup));
-
 router
 .route("/login")
 .get(userController.renderLoginForm)
