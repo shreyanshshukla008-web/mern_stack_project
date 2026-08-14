dbd2a9f1ebd6a5a847e3546e7045f6b8c0278c0f
Shreyansh Shukla <230330341+shreyanshshukla008-web@users.noreply.github.com>
2026-08-14 22:28:12 +0530

    Refactor createReview and destroyRoute methods

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,20 +1,17 @@
 const Review = require("../models/review");
 const Listing = require("../models/listing");
-
 module.exports.createReview = async(req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
     console.log(newReview.author);
     listing.reviews.push(newReview);
-
     await newReview.save();
     await listing.save();
     console.log(req.body);
     req.flash("success","Review shared successfully!");
     res.redirect(`/listings/${listing._id}`);
 };
-
 module.exports.destroyRoute = async(req,res) => {
     let { id,reviewId } = req.params;
     await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
