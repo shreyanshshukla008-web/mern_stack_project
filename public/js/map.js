4ac826bcaa062af47038e1a923de3191a61ffd5c
Shreyansh Shukla <230330341+shreyanshshukla008-web@users.noreply.github.com>
2026-08-14 22:34:12 +0530

    map.js
    Removed commented-out code for default Marker creation.

--- public/js/map.js
+++ public/js/map.js
@@ -6,8 +6,6 @@ mapboxgl.accessToken = mapToken;
         center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
-
-    // Create a default Marker and add it to the map.
     const marker = new mapboxgl.Marker({color: '#FF5A5F'})
         .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
         .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
