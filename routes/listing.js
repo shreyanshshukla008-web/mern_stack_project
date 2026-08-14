bcca9f9c06b653ccc40396f1611932cf056a239a
Shreyansh Shukla <230330341+shreyanshshukla008-web@users.noreply.github.com>
2026-08-14 22:34:48 +0530

    isting.js

--- routes/listing.js
+++ routes/listing.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn,isOwner,validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer')
 const {storage} = require("../cloudConfig.js");
