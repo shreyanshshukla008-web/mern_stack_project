d9ff41be414870976c8a0a88b0968eca474bedcc
Shreyansh Shukla <230330341+shreyanshshukla008-web@users.noreply.github.com>
2026-08-14 22:41:14 +0530

    cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,5 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
